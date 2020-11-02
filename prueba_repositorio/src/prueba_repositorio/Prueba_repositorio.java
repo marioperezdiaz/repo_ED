dcc56650611c9f3e60a53dc426a3101d7e1bec16
DAM123 <DAM123@IF03-21.iesmhp.local>
2020-11-02 16:37:13 +0100

    primera prueba github

---
+++ prueba_repositorio/src/prueba_repositorio/Prueba_repositorio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_repositorio;
+
+/**
+ *
+ * @author DAM123
+ */
+public class Prueba_repositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Prueba de repositorio GitHub");
+    }
+    
+}
